37711b0ad2a2cbf98c0f85c9cfc8a43eac9dfcb0
jainara1012 <41476970+jainara1012@users.noreply.github.com>
2021-05-05 23:07:48 +0530

    Update send_message.py

--- cowin_bot/VacciDate_bot/send_message.py
+++ cowin_bot/VacciDate_bot/send_message.py
@@ -2,12 +2,12 @@ import requests
 import json
 import telegram
 
-token = "1726606541:AAEhx3O4XsHlxlhX8u9_1E_38dS3tgnHlu8"
+token = "YOUR_TOKEN"
 url = "https://api.telegram.org/bot" + token
 
 
 def send_mess(text):
-    params = {"chat_id": "-1001177302753", "text": text}
+    params = {"chat_id": "YOUR_CHAT_ID", "text": text}
     response = requests.post(url + "/sendMessage", data=params)
     resp_dict = json.loads(response.text)
     if resp_dict.get("ok"):
