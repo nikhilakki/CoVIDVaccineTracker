f9f94aa8beab03d2a5cc0a4f8c8bd43f634c7f39
Arpit Jain <arpitkjain@deloitte.com>
2021-05-07 22:48:20 +0530

    header changes

--- cowin_bot/utils/external_caller.py
+++ cowin_bot/utils/external_caller.py
@@ -8,8 +8,11 @@ class APIInterface:
     def post(route, data=None):
         url = route
         # print(f"url = {url}, data = {data}")
+        # headers = {
+        #     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"
+        # }
         headers = {
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"
+            "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:77.0) Gecko/20190101 Firefox/77.0"
         }
         response = requests.post(url, json=data, headers=headers)
         if response.status_code != 200:
